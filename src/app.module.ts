674b795b960c9e68591f9bbec372a36b4e72ea71
Erik Stor <erik.stor.franco@gmail.com>
2023-05-23 12:11:26 -0500

    limpieza de codigo hardcodeado

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,11 @@ import {RoleRepository} from "./users/infra/repositories/role.repository";
         ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            database: 'hexagonal',
-            username: 'postgres',
-            password: '1234',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
             autoLoadEntities: true,
             synchronize: true,
             entities: [
